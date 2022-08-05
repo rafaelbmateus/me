80f244ec421c0dc2d060137d2f02d39e57ef0951
Rafael Mateus <rafaelmateus@hotmail.com.br>
2022-08-05 01:54:24 -0300

    test pwa

--- assets/js/serviceworker.js
+++ assets/js/serviceworker.js
@@ -1,22 +1,37 @@
-var staticCacheName = "pwa";
+const CACHE_NAME = "pwa";
  
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
-        "../../",
+        '../../',
         '../../index.html',
+        '../css/background.css',
+        '../css/styles.css',
       ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
     })
   );
 });
- 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
- 
+
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
