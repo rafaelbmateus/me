8222f76da9c936274b4700da143b630256087be0
Rafael Mateus <rafaelmateus@hotmail.com.br>
2022-08-04 22:55:37 -0300

    feat: paginate repos

--- assets/js/github.js
+++ assets/js/github.js
@@ -3,51 +3,32 @@ $(document).ready(function () {
 });
 
 async function repos() {
-  const url = "https://api.github.com/users/" + username + "/repos";
-  const response = await fetch(url);
-  const repos = await response.json();
   const topic_tag = "me";
+  const url = "https://api.github.com/users/" + username + "/repos";
 
-  has = false;
-  for (var i = 0; i < repos.length; i++) {
-    repo = repos[i]
-    if (repos[i].topics.includes(topic_tag)) {
-      has = true;
-      $("#repos").append(
-        `<div class="col">
-          <a href="${repo.html_url}" target="_blank">
-            <div class="card">
-              <span class="gray">${repo.language}</span>
-              <h3 class="gray">
-                <i class="fa-brands fa-buffer"></i>
-                ${repo.name}
-              </h3>
-              <p class="gray">${repo.description}</p>
-              <div class="row gray" style="font-size: 18px">
-                <div class="col">
-                  <i class="fa-solid fa-star fa-2xs"></i>
-                  ${repo.stargazers_count}
-                </div>
-                <div class="col">
-                  <i class="fa-solid fa-code-fork fa-2xs"></i>
-                  ${repo.forks_count}
-                </div>
-                <div class="col">
-                  <i class="fa-solid fa-eye fa-2xs"></i>
-                  ${repo.watchers_count}
-                </div>
-                <div class="col">
-                  ${repo.size} MB
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>`
-      );
+  page = 1;
+  count = 0;
+  while (true) {
+    const response = await fetch(url + "?page=" + page);
+    const repos = await response.json();
+
+    if (repos.length == 0) {
+      break;
     }
+
+    for (var i = 0; i < repos.length; i++) {
+      repo = repos[i]
+      if (repos[i].topics.includes(topic_tag)) {
+        if (repo.language == null) { repo.language = "Empty"; }
+        $("#repos").append(repoCard(repo))
+        count++;
+      }
+    }
+
+    page++;
   }
 
-  if (!has) {
+  if (count == 0) {
     $("#repos").append(
       `<br><br>
       <h3 align="center">
@@ -56,4 +37,37 @@ async function repos() {
       <br><br>`
     );
   }
+}
+
+function repoCard() {
+  return `
+    <div class="col">
+      <a href="${repo.html_url}" target="_blank">
+        <div class="card">
+          <span class="gray">${repo.language}</span>
+          <h3 class="gray">
+            <i class="fa-brands fa-buffer"></i>
+            ${repo.name}
+          </h3>
+          <p class="gray">${repo.description}</p>
+          <div class="row gray" style="font-size: 18px">
+            <div class="col">
+              <i class="fa-solid fa-star fa-2xs"></i>
+              ${repo.stargazers_count}
+            </div>
+            <div class="col">
+              <i class="fa-solid fa-code-fork fa-2xs"></i>
+              ${repo.forks_count}
+            </div>
+            <div class="col">
+              <i class="fa-solid fa-eye fa-2xs"></i>
+              ${repo.watchers_count}
+            </div>
+            <div class="col">
+              ${repo.size} MB
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>`;
 }
