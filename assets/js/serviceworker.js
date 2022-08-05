e23e9eaea59edac36daf7ec262c77bbab183a2e8
Rafael Mateus <rafaelmateus@hotmail.com.br>
2022-08-05 01:41:21 -0300

    test pwa

--- assets/js/serviceworker.js
+++ assets/js/serviceworker.js
@@ -4,8 +4,8 @@ self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
       return cache.addAll([
-        "/",
-        '/index.html',
+        "../../",
+        '../../index.html',
       ]);
     })
   );
